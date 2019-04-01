812dad82d84fa390ef3efb0e8a4f0986a0db2bae
TuPT <mr.tus97@gmail.com>
2019-04-01 09:26:54 +0700

    tesst change

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<p>123a</p><h3>Hello {name}!</h3>" \
+    html = "<p>123a123</p><h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
     return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
